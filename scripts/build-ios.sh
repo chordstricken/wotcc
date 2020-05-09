c0344e2043600cd5206da2addfb677cbda77740b
Jason <jason@silvermast.io>
2020-05-09 11:51:22 -0700

    2.2.0
    - Removed bower
    - Added npm commands
    - localized cordova
    - Removed some spoilers

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd $(dirname $0)/../
 
+cordova plugin save
+cordova platform rm ios
 cordova platform add ios
-cordova build ios
+cordova build ios --buildFlag="-UseModernBuildSystem=0"
 
 #plutil \
 #    -replace NSPhotoLibraryUsageDescription \
