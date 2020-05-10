919acf95dbd92c0a8a1f2952b4a1f43c249c3471
Jason <jason@silvermast.io>
2020-05-09 19:35:07 -0700

    2.2.2 ios update

--- bin/build-ios.sh
+++ bin/build-ios.sh
@@ -5,13 +5,3 @@ cordova plugin save
 cordova platform rm ios
 cordova platform add ios
 cordova build ios --buildFlag="-UseModernBuildSystem=0"
-
-#plutil \
-#    -replace NSPhotoLibraryUsageDescription \
-#    -string "Allows users to import Adventure photos and profile headshots" \
-#    platforms/ios/Awayn/Awayn-Info.plist
-#
-#plutil \
-#    -replace NSCameraUsageDescription \
-#    -string "Allows users to import Adventure photos and profile headshots" \
-#    platforms/ios/Awayn/Awayn-Info.plist
