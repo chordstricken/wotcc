bdba7c61ba0430d98e9bef5c70c3fe0192a6692f
Jason <jason@silvermast.io>
2020-09-25 11:55:52 -0700

    tee after cloudfront update to avoid less pipe

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,4 +6,4 @@ aws-sm s3 sync --delete ./www s3://wheeloftime.silvermast.io/
 PATHS_TO_INVALIDATE='/index.html /res/* /data/*'
 
 # E1IQN1E4JD6ZW1
-aws-sm cloudfront create-invalidation --distribution-id E1IQN1E4JD6ZW1 --paths "$PATHS_TO_INVALIDATE"
+aws-sm cloudfront create-invalidation --distribution-id E1IQN1E4JD6ZW1 --paths "$PATHS_TO_INVALIDATE" | tee
