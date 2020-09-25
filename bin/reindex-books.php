9f6c1bac4cd2ddf09c030786b6f36b514272649f
Jason <jason@silvermast.io>
2020-09-25 12:02:45 -0700

    fixed book 8 moridin reference

--- bin/reindex-books.php
+++ bin/reindex-books.php
@@ -5,12 +5,7 @@ foreach (glob(__DIR__ . '/../www/data/book-*') as $book) {
     foreach (glob("$book/*.json") as $file) {
         if (basename($file) === 'index.json') continue;
         $contents = file_get_contents($file);
-        $contents = preg_replace('@(\\\\n| ){2,}@', ' ', $contents);
-        $contents = preg_replace('@\\\\"@', "'", $contents);
-        $contents = preg_replace('@<\\\\@', '</', $contents);
-        $contents = preg_replace('@//+@', '/', $contents);
         $object = json_decode($contents);
-        file_put_contents($file, json_encode($object, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
         $bookIndex[$object->id] = $object;
     }
     file_put_contents("{$book}.json", json_encode($bookIndex, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
